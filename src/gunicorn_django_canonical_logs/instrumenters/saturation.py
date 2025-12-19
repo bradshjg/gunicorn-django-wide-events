03126d092a1ca49d1e7ea2ca5ed4583b9d1e377d
James Bradshaw <james.g.bradshaw@gmail.com>
2025-12-19 05:25:30 +0000

    use the proctitle to pass data about worker state
    * avoids the weirdness of shared memory
    * no longer samples worker activeness, get a snapshot of when the log was generated
    * curious to do a little benchmarking

--- src/gunicorn_django_canonical_logs/instrumenters/saturation.py
+++ src/gunicorn_django_canonical_logs/instrumenters/saturation.py
@@ -1,10 +1,66 @@
+from __future__ import annotations
+
+import dataclasses
+import os
+import re
+from functools import cached_property
+
+import psutil
+
 from gunicorn_django_canonical_logs.event_context import Context
 from gunicorn_django_canonical_logs.instrumenters.protocol import InstrumenterProtocol
 from gunicorn_django_canonical_logs.instrumenters.registry import register_instrumenter
-from gunicorn_django_canonical_logs.monitors.saturation import CurrentSaturationStats
+
+
+@dataclasses.dataclass
+class WorkerStatus:
+    count: int
+    active: int
 
 
 @register_instrumenter
 class SaturationInstrumenter(InstrumenterProtocol):
+    arbiter_regex = re.compile(r"gunicorn: master .* \[backlog: (\d+)\]")
+    worker_regex = re.compile(r"gunicorn: worker .* \[status: (idle|busy)\]")
+
+    @cached_property
+    def parent_process(self) -> psutil.Process:
+        return psutil.Process(os.getppid())
+
+    @property
+    def worker_processes(self) -> list[psutil.Process]:
+        return self.parent_process.children()
+
+    @property
+    def backlog(self) -> str:
+        if match := self.arbiter_regex.match(self.parent_process.cmdline()[0]):
+            return match.group(1)
+
+    def active(self, worker_process: psutil.Process) -> bool:
+        if match := self.worker_regex.match(worker_process.cmdline()[0]):
+            return match.group(1) == "busy"
+        return False
+
+    @property
+    def worker_status(self) -> WorkerStatus | None:
+        try:
+            status = list(map(self.active, self.worker_processes))
+            return WorkerStatus(
+                count=len(status),
+                active=sum(status),
+            )
+        except:
+            pass
+
+
     def call(self, _req, _resp, _environ):
-        Context.update(namespace="g", context=CurrentSaturationStats.get())
+        saturation_data = {
+            "backlog": self.backlog,
+        }
+        worker_status = self.worker_status
+        if worker_status:
+            saturation_data.update({
+                "w_count": worker_status.count,
+                "w_active": worker_status.active,
+            })
+        Context.update(namespace="g", context=saturation_data)
