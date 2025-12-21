c5552dc8229baafd6972daa638e73e5310866c8d
James Bradshaw <james.g.bradshaw@gmail.com>
2025-12-21 23:50:00 +0000

    add integration test and wire up the instrumenter

--- src/gunicorn_django_canonical_logs/instrumenters/__init__.py
+++ src/gunicorn_django_canonical_logs/instrumenters/__init__.py
@@ -8,6 +8,7 @@ from gunicorn_django_canonical_logs.instrumenters import (  # noqa F401 register
     database,
     exception,
     request,
+    http,
 )
 from gunicorn_django_canonical_logs.instrumenters.registry import instrumenter_registry
 
